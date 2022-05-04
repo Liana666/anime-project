1e95d0e3a836e6d9ddc8a7fc878c53cd74c8ce91
Liana <liana.nikitina98@gmail.com>
2022-05-04 10:40:29 +0300

    transformResponse filter and search

--- src/store/api/animeApi.ts
+++ src/store/api/animeApi.ts
@@ -8,7 +8,7 @@ import { Genres } from '../../types/types';
 
 export const animeApi = createApi({
     reducerPath: "animeApi",
-    baseQuery: fetchBaseQuery({baseUrl:"https://api.aniapi.com/v1/"}),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://api.aniapi.com/v1/" }),
     endpoints: (build) => ({
         getRandomAnime: build.query({
             query: (count: number) => `random/anime/${count}`,
@@ -16,44 +16,48 @@ export const animeApi = createApi({
                 const anime = response.data.filter(itemAnime => itemAnime.descriptions.en && itemAnime.titles.en);
 
                 return anime;
-              },
+            },
         }),
 
         getGenres: build.query({
             query: (type: number) => `resources/1.0/${type}`,
             transformResponse: (response: { data: Genres }) => {
                 return response.data.genres;
-              },
+            },
         }),
 
-        getFilter: build.query<ItemAnime[] , AnimeResponse>({
-            query: ({year, genres}) => `anime?&year=${year}&genres=${genres}`,
+        getFilter: build.query<ItemAnime[], AnimeResponse>({
+            query: ({ year, genres }) => `anime?&year=${year}&genres=${genres}`,
             transformResponse: (response: { data: SearchAndFilterData }) => {
-                return response.data.documents;
-              },
+                const anime = response.data.documents.filter(itemAnime => itemAnime.descriptions.en && itemAnime.titles.en);
+
+                return anime;
+            },
         }),
 
         searchAnime: build.query<ItemAnime[], string | undefined>({
             query: (title) => `anime?title=${title}`,
             transformResponse: (response: { data: SearchAndFilterData }) => {
-                return response.data.documents;
-              },
+                const anime = response.data.documents.filter(itemAnime => itemAnime.descriptions.en && itemAnime.titles.en);
+
+                return anime;
+            },
         }),
 
         getItemAnime: build.query<ItemAnime, number | string>({
             query: (id) => `anime/${id}`,
             transformResponse: (response: { data: ItemAnime }) => {
-                const descriptions =  response.data.descriptions;
-                const descriptionsParse:any = descriptions.en && parse( descriptions.en);
-                
-                response.data.descriptions.en = typeof descriptionsParse === "object" 
+                const descriptions = response.data.descriptions;
+                const descriptionsParse: any = descriptions.en && parse(descriptions.en);
+
+                response.data.descriptions.en = typeof descriptionsParse === "object"
                     ? descriptionsParse
                         ?.filter((item: any) => typeof item === "string")
-                        .join("") 
+                        .join("")
                     : descriptionsParse;
-            
+
                 return response.data;
-              },
+            },
         })
-    })  
+    })
 })
